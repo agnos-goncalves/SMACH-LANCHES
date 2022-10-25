4dcd162b430a24e050376737771e9b24303e197f
agnos <agnos.goncalves@gmail.com>
2022-10-25 10:03:03 -0300

    add events to buttons action

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,28 @@ function changePage(pageState) {
 
 window.onload = () => {
   const buttonAddNewOrder = document.querySelector(".form-add-order .btn");
+  const buttonAddProductToOrder = document.querySelector(
+    ".form-new-order .btn-primary"
+  );
+  const buttonSaveOrders = document.querySelector(".table-new-order .btn-cta");
 
   buttonAddNewOrder.addEventListener("click", () => {
+    e.preventDefault();
+    e.stopPropagation();
     changePage(PAGE_STATE.NEW_ORDER);
   });
+
+  buttonAddProductToOrder.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    alert();
+  });
+
+  buttonSaveOrders.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    changePage(PAGE_STATE.ALL_ORDERS);
+  });
+
+  changePage(PAGE_STATE.NEW_ORDER);
 };
